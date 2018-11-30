c1f3f43b78c666b980fb04d89ca4c803f99e75b3
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-11-30 21:59:58 +0100

    db...

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../handlers/db.js');
 
 router.get('/', (req, res) => {
     res.send('Dzięki za wizytę!');
