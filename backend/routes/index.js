9c8a34a87da3af605a0581898dcbfa83d8270ce4
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-11-30 22:24:14 +0100

    example data on /

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,8 +5,18 @@ const db = require('../handlers/db.js');
 
 router.get('/', (req, res) => {
     res.json({
+        success: true,
+        stopline: {
+            id: 2,
+            line_name: 'jp2',
+            stop_name: 'xDDDDDd',
+            ord: 4,
+            direction: false,
+        },
         id: 666,
-        
+        rating: -2,
+        time: new Date().getTime(),
+        desc: 'tramwaj się zepsuł lol'
     });
 });
 
