aec752cf3e734db74a9adb949b1ee96f465ffcec
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-11-30 23:09:18 +0100

    added required future functions for db

--- backend/handlers/db.js
+++ backend/handlers/db.js
@@ -1,7 +1,6 @@
 const mariadb = require('mariadb');
 const credentials = JSON.parse(require('fs').readFileSync('tokens/dbCredentials.json'));
 
-
 const pool = mariadb.createPool({
     host: credentials.host,
     port: credentials.port,
@@ -20,6 +19,28 @@ pool.getConnection().then(conn => {
 
 module.exports = {
     fetchAccidents: function() {
-        return [];
-    }
+        pool.getConnection().then(conn => {
+            conn.query(
+                'SELECT test FROM test2 WHERE id = ? AND time = ?',
+                ['1, 2']
+                ).then(rows => {
+                //do something with the rows and return it
+            }).catch(err => {
+                conn.close();
+                throw err;
+            });
+        }).catch(err => {
+            conn.close();
+            throw err;
+        });
+    },
+    //TODOs
+    fetchAccident: function(accidentId) {},
+    voteForAccident: function(accidentId, up) {}, //(int, bool)
+    addAccident: function(accident) {}, //(object)
+
+    fetchInspections: function() {},
+    fetchInspection: function(inspectionId) {},
+    voteForInspection: function(inspectionId, up) {},
+    addInspection: function(inspection) {}
 };
