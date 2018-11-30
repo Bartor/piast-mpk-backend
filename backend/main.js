248d6714f18536ff6f1e2a691bb4727d8ec2b0f9
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-11-30 21:09:51 +0100

    http changes

--- backend/main.js
+++ backend/main.js
@@ -5,18 +5,18 @@ const fs = require('fs');
 const path = require('path');
 
 const http = require('http');
-const https = require('https');
+//const https = require('https');
 
 //routers
 const indexRouter = (require(path.join(__dirname, 'routes/index.js')));
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/bartor.net/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/bartor.net/fullchain.pem', 'utf8');
+//const privateKey = fs.readFileSync('/etc/letsencrypt/live/bartor.net/privkey.pem', 'utf8');
+//const certificate = fs.readFileSync('/etc/letsencrypt/live/bartor.net/fullchain.pem', 'utf8');
 
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
+//const credentials = {
+//    key: privateKey,
+//    cert: certificate
+//};
 
 let app = express();
 
@@ -24,19 +24,21 @@ app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 
-const httpServer = http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-});
+// const httpServer = http.createServer((req, res) => {
+//     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//     res.end();
+// });
 
-const httpsServer = http.createServer(credentials, app);
+// const httpsServer = http.createServer(credentials, app);
+
+const httpServer = http.createServer(app);
 
 let port = (process.env.PORT || 2137);
 
-httpServer.listen(3000, () => {
-    console.log('HTTP started on 3000');
+httpServer.listen(port, () => {
+    console.log('HTTP started on ' + port);
 });
 
-httpsServer.listen(2137, () => {
-    console.log('HTTPS started on 2137');
-})
+// httpsServer.listen(443, () => {
+//     console.log('HTTPS started on 2137');
+// })
