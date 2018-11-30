bd01992e6af50be9267f7a87f722e08caa739466
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-01 00:23:20 +0100

    basic auth test

--- backend/handlers/auth.js
+++ backend/handlers/auth.js
@@ -10,11 +10,27 @@ admin.initializeApp({
 module.exports = {
     cachedUsers: {},
     verifyToken: function(token) {
+        if (this.cachedUsers.hasOwnProperty(token) && this.cachedUsers[token].exp > new Date().getTime()) {
+            return true;
+        }
         admin.auth().verifyIdToken(token).then(decodedToken => {
-            return decodedToken;
+            let uid = decodedToken.uid;
+            this.cachedUsers[uid] = {};
+            admin.auth().getUser(uid).then(userRecord => {
+                userRecord.exp = decodedToken.exp;
+                userRecord.token = token;
+                Object.assign(userRecord, this.cachedUsers[uid]);
+                setTimeout((uid) => {
+                    delete this.cachedUsers[uid];
+                }, 3600000);
+                return true;
+            }).catch(err => {
+                console.log(err.stack);
+                return false;
+            });
         }).catch(err => {
             console.log(err.stack);
-            return null;
+            return false;
         });
     }
 };
