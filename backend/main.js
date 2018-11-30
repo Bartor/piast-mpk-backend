f2c5279aa5c9bfcdb8a628fa7b00763a3197923e
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-11-30 20:59:15 +0100

    trying https

--- backend/main.js
+++ backend/main.js
@@ -4,9 +4,18 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const http = require('http');
+const https = require('https');
 
 //routers
 const indexRouter = (require(path.join(__dirname, 'routes/index.js')));
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/bartor.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/bartor.net/fullchain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate
+};
 
 let app = express();
 
@@ -14,9 +23,19 @@ app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 
-let server = http.createServer(app);
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+});
+
+const httpsServer = http.createServer(credentials, app);
+
 let port = (process.env.PORT || 2137);
 
-server.listen(port, () => {
-    console.log('Server started on ' + port);
+httpServer.listen(3000, () => {
+    console.log('HTTP started on 3000');
 });
+
+httpsServer.listen(2137, () => {
+    console.log('HTTPS started on 2137');
+})
