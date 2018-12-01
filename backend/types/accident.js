0903697919bf74d9df3f1c1a689db027e737b7e2
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-01 05:56:50 +0100

    type update

--- backend/types/accident.js
+++ backend/types/accident.js
@@ -1,6 +1,7 @@
 class Accident {
-    constructor(stopline, user, description) {
+    constructor(stopline, direction, user, description) {
         this.stopline = stopline;
+        this.direction = direction;
         this.time = Math.round(new Date()/1000);
         this.user = user;
         this.description = description;
