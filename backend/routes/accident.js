8ff14466f8e80c38688c16bae720486506f9eaa5
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-01 06:01:08 +0100

    all accident endpoints hopefully working

--- backend/routes/accident.js
+++ backend/routes/accident.js
@@ -7,24 +7,29 @@ const Accident = require('../types/accident.js');
 const auth = require('../handlers/auth.js');
 
 router.get('/', (req, res) => {
-    db.fetchAccidents((err, rows) => {
-        if (err) {
-            console.log(err.stack);
-            res.status(500).send();
-        } else {
-            res.json(rows);
-        }
+    auth.verifyToken(req.token).then(output => {
+        db.fetchAccidents((err, rows) => {
+            if (err) {
+                console.log(err.stack);
+                res.status(500).send();
+            } else {
+                res.json(rows);
+            }
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(401).send();
     });
 });
 
 router.post('/', (req, res) => {
     if (req.body) {
-        if (!req.body.stopline && req.body.description) {
+        if (!req.body.stop_line_id || !req.body.description) {
             res.status(400).send('stopline and description in body required');
             return;
         }
         auth.verifyToken(req.token).then(output => {
-            db.addAccident(new Accident(req.body.stopline, u.uid, req.description), (err, rows) => {
+            db.addAccident(new Accident(req.body.stop_line_id, u.uid, req.description), (err, rows) => {
                 if (err) {
                     console.log(err.stack);
                     res.status(500).send();
@@ -43,13 +48,22 @@ router.post('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     if (req.body) {
-        db.fetchAccident(req.params.id, (err, rows) => {
-            if (err) {
-                console.log(err.stack);
-                res.status(500).send();
-            } else {
-                res.json(rows);
-            }
+        if(!req.params.id) {
+            res.status(400).send('/id is required');
+            return;
+        }
+        auth.verifyToken(req.token).then(output => {
+            db.fetchAccident(req.params.id, (err, rows) => {
+                if (err) {
+                    console.log(err.stack);
+                    res.status(500).send();
+                } else {
+                    res.json(rows);
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(401).send();
         });
     } else {
         res.status(400).send('Content-Type should be application/json');   
@@ -58,13 +72,25 @@ router.get('/:id', (req, res) => {
 
 router.patch('/:id', (req, res) => {
     if (req.body) {
-        db.voteForAccident(req.params.id, -1, (err, rows) => {
-            if (err) {
-                console.log(err.stack);
-                res.status(500).send();
-            } else {
-                res.json(rows);
-            }
+        if(!req.params.id) {
+            res.status(400).send('/id is required');
+            return;
+        } else if (!req.body.rating || Math.abs(req.body.rating) != 1) {
+            res.status(400).send('rating in body in value of -1 or 1 is required');
+            return;
+        }
+        auth.verifyToken(req.token).then(output => {
+            db.voteForAccident(req.params.id, req.body.rating, (err, rows) => {
+                if (err) {
+                    console.log(err.stack);
+                    res.status(500).send();
+                } else {
+                    res.json(rows);
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(401).send();
         });
     } else {
         res.status(400).send('Content-Type should be application/json');   
