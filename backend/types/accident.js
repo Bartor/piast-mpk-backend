6cdd51bda5105ac7d694328ec7c80a6b751e6528
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2018-12-01 01:54:49 +0100

    added type definitions

---
+++ backend/types/accident.js
@@ -0,0 +1,11 @@
+class Accident {
+    constructor(stopline, rate, time, user, description) {
+        this.stopline = stopline;
+        this.rate = rate;
+        this.time = time;
+        this.user = user;
+        this.description = description;
+    }
+}
+
+module.exports = Accident;
---
+++ backend/types/inspection.js
@@ -0,0 +1,10 @@
+class Inspection {
+    constructor(stopline, rate, time, user) {
+        this.stopline = stopline;
+        this.rate = rate;
+        this.time = time;
+        this.user = user;
+    }
+}
+
+module.exports = Inspection;
